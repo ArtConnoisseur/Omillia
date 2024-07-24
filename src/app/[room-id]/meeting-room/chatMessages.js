b4c038a4e31676bdb63b7bd17e5524b74779f3a4
mav <Maverickodes.mac@gmail.com>
2024-07-24 23:33:11 +0530

    Fixed Right Section. Cards of people in call is still not fixed. Buttons to be fixed.

---
+++ src/app/[room-id]/meeting-room/chatMessages.js
@@ -0,0 +1,78 @@
+const chatMessages = [
+    {
+        name: "John Doe",
+        gender: "boy",
+        message: "Hey there! How are you doing?",
+        time: "10:00 AM",
+    },
+    {
+        name: "Jane Smith",
+        gender: "girl",
+        message: "I'm doing great, thanks! How about you?",
+        time: "10:05 AM",
+    },
+    {
+        name: "John Doe",
+        gender: "boy",
+        message: "Not too bad. Did you catch the game last night?",
+        time: "10:07 AM",
+    },
+    {
+        name: "Jane Smith",
+        gender: "girl",
+        message: "Yeah, it was amazing! That last-minute goal was incredible.",
+        time: "10:10 AM",
+    },
+    {
+        name: "Alice Johnson",
+        gender: "girl",
+        message: "Good morning everyone! What are you talking about?",
+        time: "10:12 AM",
+    },
+    {
+        name: "John Doe",
+        gender: "boy",
+        message: "Morning, Alice! Just chatting about the game last night.",
+        time: "10:15 AM",
+    },
+    {
+        name: "Alice Johnson",
+        gender: "girl",
+        message: "Oh, I missed it. Sounds like I need to watch the highlights.",
+        time: "10:18 AM",
+    },
+    {
+        name: "Bob Brown",
+        gender: "boy",
+        message: "Hi folks, what's up?",
+        time: "10:20 AM",
+    },
+    {
+        name: "Jane Smith",
+        gender: "girl",
+        message:
+            "Hey Bob! We were just talking about last night's game. Did you watch it?",
+        time: "10:22 AM",
+    },
+    {
+        name: "Bob Brown",
+        gender: "boy",
+        message: "I did! That was one of the best games I've seen in a while.",
+        time: "10:25 AM",
+    },
+    {
+        name: "Charlie Green",
+        gender: "boy",
+        message: "Hi everyone! Sounds like I missed a great game.",
+        time: "10:28 AM",
+    },
+    {
+        name: "John Doe",
+        gender: "boy",
+        message:
+            "Hey Charlie! You definitely did. We should all watch the highlights together later.",
+        time: "10:30 AM",
+    },
+];
+
+export default chatMessages;
